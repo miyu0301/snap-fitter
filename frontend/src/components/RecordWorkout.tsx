f8ed6d363a4b86eb71a2bffb1e4943961d644293
MACBOOK PRO <macbookpro@MacBook-Pro.local>
2024-04-10 13:10:07 -0700

    add timer and navigation bar

--- frontend/src/components/RecordWorkout.tsx
+++ frontend/src/components/RecordWorkout.tsx
@@ -1,59 +1,59 @@
-//import { Link } from 'react-router-dom'
-import {Button, Form} from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Form } from 'react-bootstrap';
 import UserNavbar from '../partials/navbar';
-import { useState } from 'react';
 import Timer from './Timer';
 
-const RecordWorkout = () => {
-
+const RecordWorkout: React.FC = () => {
   const [showTimer, setShowTimer] = useState(false);
+  const [workoutTime, setWorkoutTime] = useState(0);
+  const [showWorkoutTime, setShowWorkoutTime] = useState(false);
 
   const handleStartClick = () => {
     setShowTimer(true);
   };
 
-  const handleEndClick = () => {
+  const handleEndClick = (timeElapsed: number) => {
     setShowTimer(false);
+    setWorkoutTime(timeElapsed);
+    setShowWorkoutTime(true);
   };
 
   return (
     <>
-    <UserNavbar />
-    <div className='d-flex align-items-center justify-items-center'>
-          <div className='home-bg-image bg-cover-center col-50'></div>
-          <div className='col-50'>
-              <div className="container text-container vertical-center-form">
-                {!showTimer ? (
-                <div>
+      <UserNavbar />
+      <div className='d-flex align-items-center justify-items-center'>
+        <div className='home-bg-image bg-cover-center col-50'></div>
+        <div className='col-50'>
+          <div className="container text-container vertical-center-form">
+            {!showTimer && !showWorkoutTime ? (
+              <div>
                 <h2 className='anton-regular txt-md mb-4 text-center'>Record workout</h2>
                 <p>Select workout:</p>
                 <Form.Select aria-label="Default select example">
-                    <option>Select type of activity</option>
-                    <option value="1">Outdoor Walk</option>
-                    <option value="2">Core Training</option>
-                    <option value="3">Elliptical</option>
-                    <option value="4">Indoor Run</option>
-                    <option value="5">Outdoor Run</option>
-                    <option value="6">Outdoor Cycle</option>
-                    <option value="7">Hiking</option>
-                    <option value="8">Yoga</option>
-                    <option value="9">Open Water Swim</option>
+                  <option>Select type of activity</option>
+                  <option value="1">Outdoor Walk</option>
+                  <option value="2">Core Training</option>
+                  <option value="3">Elliptical</option>
+                  <option value="4">Indoor Run</option>
+                  <option value="5">Outdoor Run</option>
+                  <option value="6">Outdoor Cycle</option>
+                  <option value="7">Hiking</option>
+                  <option value="8">Yoga</option>
+                  <option value="9">Open Water Swim</option>
                 </Form.Select>
                 <div className="text-right">
-                <button onClick={handleStartClick}>Start</button>
+                  <button className='button btn-solid w-50 mt-4' onClick={handleStartClick}>Start</button>
                 </div>
-                </div>
-                ) : (
-                  <Timer onEnd={handleEndClick} />
-                )}
               </div>
+            ) : null}
+            {showTimer && !showWorkoutTime && <Timer onEnd={handleEndClick} />}
+            {showWorkoutTime && 
+            <p>Total workout time: {workoutTime} seconds <br></br>Calories burned: ???</p>}
           </div>
+        </div>
       </div>
+    </>
+  );
+};
 
-      
-
-      </>
-  )
-}
-
-export default RecordWorkout
+export default RecordWorkout;
--- frontend/src/components/Timer.tsx
+++ frontend/src/components/Timer.tsx
@@ -1,18 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Timer = ({ onEnd }) => {
-  const [showMainComponent, setShowMainComponent] = useState(true);
+interface TimerProps {
+  onEnd: (timeElapsed: number) => void;
+}
 
+const Timer: React.FC<TimerProps> = ({ onEnd }) => {
+  const [timeElapsed, setTimeElapsed] = useState(0);
+  const [isActive, setIsActive] = useState(true);
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setTimeElapsed((prevTimeElapsed) => prevTimeElapsed + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive]);
+
+  const handleEndClick = () => {
+    const currentTimeElapsed = timeElapsed; // Almacenar el tiempo transcurrido actual
+    setIsActive(false);
+    onEnd(currentTimeElapsed); // Pasar el tiempo transcurrido almacenado
+  };
+
+  // Función para formatear el tiempo a formato HH:MM:SS
+  const formatTime = (time: number): string => {
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time % 3600) / 60);
+    const seconds = time % 60;
+
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  };
 
   return (
     <>
-      {showMainComponent ? (
-        <div>
-          <h2>Timer Component</h2>
-          {/* Aquí va el contenido del temporizador */}
-          <button onClick={onEnd}>End</button>
-        </div>
-      ) : null}
+      <div className="text-center m-auto">
+        <div className="timer">{formatTime(timeElapsed)}</div>
+        <button className='button btn-solid mt-4' onClick={handleEndClick}>End Workout</button>
+      </div>
     </>
   );
 };
